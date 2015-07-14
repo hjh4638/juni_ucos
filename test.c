93831546cc2c2ea55c17d8e068a2761b29aed594
juni <hjh4638@naver.com>
2015-07-15 08:35:45 +0900

    semapore USE

--- test.c
+++ test.c
@@ -6,30 +6,44 @@ OS_STK MyChildStack[1024];
 void MyTask(void* pdata);
 void MyChildTask(void* pdata);
 
+int res;
+OS_EVENT* sem;
+
 int  main (void)
 {
 	int a;
 	printf("main address = %d\n",&a);
-    OSInit();
-    OSTaskCreate(MyTask, (void *)0, &MyTaskStack[1023], 10);
-    OSStart();
-    return 0;
+	OSInit();
+	OSTaskCreate(MyTask, (void *)0, &MyTaskStack[1023], 10);
+	OSTaskCreate(MyChildTask, (void*) 20, &MyChildStack[1023], 20);
+	OSStart();
+	return 0;
 }
 
 
 void  MyTask (void *pdata)
 {
-	OSTaskCreate(MyChildTask, (void*) 20, &MyChildStack[1023], 20);
-	printf("start address = %d, end address = %d\n", MyTaskStack, &MyTaskStack[1023]);
+	INT8U err;
+	sem = OSSemCreate(1);
+
+	
 	while(1){
-		printf("10task\n");
-		OSTaskSuspend(10);
+		OSTimeDly(1);
+		OSSemPend(sem,0,&err);
+		res = 0;
+		OSSemPost(sem);
+		OSTimeDly(1);
 	}
+
 }
 void MyChildTask(void* pdata){
+	INT8U err;
 	while(1){
-		printf("20 task\n");
-		OSTaskResume(10);
+		OSSemPend(sem,0,&err);
+		res = 3;
 		OSTimeDly(1);
+		res+=5;
+		printf("Result = %d\n", res);
+		OSSemPost(sem);
 	}
 }
